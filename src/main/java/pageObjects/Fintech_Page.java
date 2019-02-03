ef7119d9871da3422365aad75a95ef6352deb3de
Murp01 <pmurphy35@hotmail.com>
2019-02-03 10:58:20 +0000

    Fintech: Working on method that clicks only on displayed elements
    I can get it to click but I want to right click.

--- src/main/java/pageObjects/Fintech_Page.java
+++ src/main/java/pageObjects/Fintech_Page.java
@@ -5,13 +5,16 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 public class Fintech_Page extends BasePage{
 	
 	String fintechLocationSearchXpath = "//select[@title='Select Location']/option";
 	String fintechRetrievedLawyers = "//div[@id='key-contact-results-232f3fad-2271-4c52-8057-fe2c7cf98888']//div[@class='padding-top-10 padding-bottom-10']";
+	String fintechRetrievedLawyersHref = "//div[@class='key-contact-results']//div[@class='padding-top-10 padding-bottom-10']/a";
 	
 	public @FindBy(xpath = "//button[@title='All Locations']") WebElement button_PeopleSearchLocationDropdown;
 	public @FindBy(xpath = "//div[@id='key-contact-results-232f3fad-2271-4c52-8057-fe2c7cf98888']") WebElement container_RetrievedFintechLawyers;
@@ -22,33 +25,34 @@ public class Fintech_Page extends BasePage{
 	}
 	
 	
-	public void assertAllFilteredFintechLawyersLocations() {
-/*		Capture all retrieved lawyers, count integer and use for a for loop.  The for loop will capture a method
-		that will open profile into a new tab. In the profile assert the location matches the parameter, then 
-		close the tab, select next lawyer*/
-/*		List<WebElement> filteredLawyers = driver.findElements(
-				By.xpath("//div[@id='key-contact-results-232f3fad-2271-4c52-8057-fe2c7cf98888']//div[@class='padding-top-10 padding-bottom-10']"));
-		int visibleFintechLawyer = 0;
-		if (filteredLawyers.size() > 0) {
-			for (WebElement we: filteredLawyers) {
-				if (we.isDisplayed()) {
-					visibleFintechLawyer++;
-				}
-			}
-			System.out.println("There were " + visibleFintechLawyer + " Fintech Lawyers");
-		}
-		else {
-			System.out.println("There were no retrieved Fintech Lawyers");
-		}*/
-		
-		System.out.println(countVisibleElements(fintechRetrievedLawyers));
+	public void assertAllFilteredFintechLawyersLocations() throws InterruptedException {
+		clickOnOnlyDisplayedElement(fintechRetrievedLawyersHref);
 		
 	}
 	
 	
+	public void clickOnOnlyDisplayedElement(String xpath) throws InterruptedException {
+		List<WebElement> filteredLawyers = driver.findElements(By.xpath(xpath));
+
+		if (filteredLawyers.size() > 0) {
+			for (WebElement we: filteredLawyers) {
+				if (we.isDisplayed()) {
+					//jsClick(we);
+					Actions action = new Actions(getDriver());
+					action.contextClick(we).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform();
+					Thread.sleep(3000);
+				}
+			}
+		}
+		else {
+			System.out.println("Filtered Lawyer click failed");
+		}
+			
+	}
+	
+	
 	public int countVisibleElements(String xpath) {
-		List<WebElement> filteredLawyers = driver.findElements(
-				By.xpath(xpath));
+		List<WebElement> filteredLawyers = driver.findElements(By.xpath(xpath));
 		int visibleElements = 0;
 		if (filteredLawyers.size() > 0) {
 			for (WebElement we: filteredLawyers) {
